acbd88e470d6b27ee8652d82edc87e9ac1f67394
Dino Livanos <dinolivanos+git@gmail.com>
2018-05-28 13:28:07 +0100

    models

--- pagemetrics/metricsapp/models.py
+++ pagemetrics/metricsapp/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+SUCCESS = 'SUCCESS'
+PENDING = 'PENDING'
+STATES = frozenset({SUCCESS, PENDING})
+TASK_STATE_CHOICES = sorted(zip(STATES, STATES))
+
+
+class Metric(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Page(models.Model):
+    url = models.URLField()
+    description = models.TextField()
+
+    reports = models.ManyToManyField(Metric, through='Report')
+
+    def __str__(self):
+        return self.url
+
+
+class Report(models.Model):
+    page = models.ForeignKey(Page, on_delete=models.CASCADE)
+    metric = models.ForeignKey(Metric, on_delete=models.CASCADE)
+    status_date = models.DateTimeField()
+    status = models.CharField(
+        'state',
+        max_length=50, default=PENDING,
+        choices=TASK_STATE_CHOICES,
+    )
+
+    report = models.URLField()
+
+    class Meta:
+        ordering = ["-status_date"]
+
+    def __str__(self):
+        return '{}, {}, {}'.format(self.status_date, self.metric, self.page)
--- pagemetrics/metricsapp/tests.py
+++ pagemetrics/metricsapp/tests.py
@@ -1,3 +1,64 @@
 from django.test import TestCase
 
 # Create your tests here.
+import datetime
+import pytz
+from .models import *
+
+
+class MetricTestCase(TestCase):
+
+    def setUp(self):
+        Metric.objects.create(name='metric', description='description')
+
+    def test_metric(self):
+        metric = Metric.objects.get(name='metric')
+        self.assertEqual(metric.name, 'metric')
+        self.assertEqual(metric.description, 'description')
+
+
+class PageTestCase(TestCase):
+
+    def setUp(self):
+        Page.objects.create(url='http://www.test.com', description='description')
+
+    def test_metric(self):
+        page = Page.objects.get(url='http://www.test.com')
+        self.assertEqual(page.url, 'http://www.test.com')
+        self.assertEqual(page.description, 'description')
+
+
+class ReportTestCase(TestCase):
+    success_date = datetime.datetime(2018, 12, 2, tzinfo=pytz.utc)
+    def setUp(self):
+        metric = Metric.objects.create(name='metric', description='description')
+        page = Page.objects.create(url='http://www.test.com', description='description')
+
+        report1 = Report.objects.create(page=page,
+                                        metric=metric,
+                                        status_date=datetime.datetime(2018, 12, 1, 0, 0, tzinfo=pytz.utc),
+                                        status=SUCCESS,
+                                        report='file:///report/here')
+
+
+        report2 = Report.objects.create(page=page,
+                                        metric=metric,
+                                        status_date=ReportTestCase.success_date,
+                                        status=PENDING,
+                                        report='file:///report2/here')
+
+    def test_page_reports(self):
+        page = Page.objects.get(url='http://www.test.com')
+
+        reports = page.reports.all()
+        self.assertEqual(reports.count(), 2)
+
+    def test_reports_status(self):
+        pending_reports = Report.objects.filter(status=PENDING)
+        self.assertEqual(pending_reports.count(), 1)
+        self.assertEqual(pending_reports[0].status, PENDING)
+        self.assertEqual(pending_reports[0].status_date, ReportTestCase.success_date)
+
+        success_reports = Report.objects.filter(status=SUCCESS)
+        self.assertEqual(success_reports.count(), 1)
+        self.assertEqual(success_reports[0].status, SUCCESS)
